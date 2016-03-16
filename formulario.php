1e9991698678d4263e6a9c77bebb16a3285557b6
Cesar Ramiro Beltrán Hernandez <cesar-beltran-85@hotmail.es>
2016-03-16 18:43:10 -0500

    avance1
    Cuerpo del script en html

--- formulario.php
+++ formulario.php
@@ -1,1 +1,16 @@
 <!DOCTIPE html>
+<<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Env&iacute;o de datos por GET y POST</title>
+	<meta charset="utf8"/>
+</head>
+
+
+<body>
+
+
+
+
+</body>
+</html>
